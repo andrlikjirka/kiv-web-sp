5c257b06c30fb38c2201b845c121d15141c3661c
Jiří Andrlík <andrlik.jirka@gmail.com>
2021-10-30 15:01:18 +0200

    add Login.class.php Pridana trida pro spravu prihlasovani uzivatele

---
+++ Login_Registrace/Login.class.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Trida pro spravu prihlaseni uzivatele
+ */
+class Login
+{
+    /** @var Database $db Objekt pro praci s databazi */
+    private $db;
+
+    /** @var Session $session Objekt pro praci se session */
+    private $session;
+
+    /** @var string Klic, pod kterym budu pristupovat do session */
+    private const SESSION_KEY = "logged_user_id"; //cim budu pristupovat do Session
+
+    /**
+     * Konstruktor inicializuje objekt Session a ziska pripojeni k databazi
+     */
+    public function __construct()
+    {
+        require_once ("../Session_Cookies/Session.class.php");
+        $this->session = Session::getSession();
+        require_once ("../Database/Database.class.php");
+        $this->db = Database::getDBConnection();
+    }
+
+    /**
+     * Otestuje, zda je uzivatel prihlasen (zda je zaznam v session)
+     * @return bool Vrati true pokud je uzivatel prihlasen, jinak false
+     */
+    public function isUserLoggedIn()
+    {
+        return $this->session->isSessionSet(self::SESSION_KEY);
+    }
+
+    /**
+     * Funkce zajistuje prihlaseni uzivatele
+     * @param string $login Login uzivatele
+     * @param string $password Heslo uzivatele
+     * @return bool Vrati true pokud prihlaseni uzivatele probehne uspesne, jinak false
+     */
+    public function login(string $login,string $password)
+    {
+        // ziskam uzivatele z DB - primo overuji login
+        $whereStatement = "login='$login'";
+        $user = $this->db->selectFromTable(TABLE_UZIVATELE, "*", $whereStatement); //nahrazeni funkcí pro ziskani zašifrovaného hesla?
+        //ziskal jsem uzivatele
+        if (count($user)) {
+            if (password_verify($password, $user[0]['heslo'])) {
+                //ziskal - ulozim jeho ID do Session
+                $this->session->setSession(self::SESSION_KEY, $user[0]['id_uzivatel']); //beru prvniho nalezeneho a ukladam jen jeho ID
+                return true;
+            } else {
+                echo "ERROR: Zadane heslo není správné.";
+                return false;
+            }
+        } else {
+            //neziskal jsem uzivatele
+            return false;
+        }
+    }
+
+    /**
+     * Funkce odhlasi uzivatele
+     */
+    public function logout()
+    {
+        $this->session->removeSession(self::SESSION_KEY);
+    }
+
+    /**
+     * Funkce ziska data prihlaseneho uzivatele
+     * @return mixed|null Vrati data uzivatele, nebo null
+     */
+    public function getLoggedUserData()
+    {
+        if ($this->isUserLoggedIn()) {
+            //ziskam id uzivatele ze session
+            $userId = $this->session->readSession(self::SESSION_KEY);
+            //pokud nemam id uzivatele, tak vypisu chybu a vynutim odhlaseni
+            if ($userId == null) {
+                //nemam id uzivatele ze session - vypisu chybu, uzivatele odhlasim, vratim null
+                echo "SERVER ERROR: Data prihlaseneho uzivatele nenalezena, a proto byl uživatel odhlášen";
+                $this->logout();
+                return null;
+            } else {
+                //nactu data uzivatele z db
+                $userData = $this->db->getUserByID($userId); //ziskam udaje o jednom uzivateli s danym id (ale v DB muze byt jen jeden uzivatel s danym loginem, takze OK)
+                //mam data uzivatele?
+                if (empty($userData)) {
+                    //nemam
+                    echo "ERROR: Data přihlášeného uživatele se nenachází v DB, a proto byl uživatel odhlášen.";
+                    $this->logout();
+                    return null;
+                } else {
+                    //protoze DB vraci pole uzivatelu, tak vratim jeho prvni polozku (prvniho uzivatele)
+                    return $userData;
+                }
+            }
+        } else {
+            //uzivatel neni prihlasen
+            return null;
+        }
+    }
+
+
+
+}
