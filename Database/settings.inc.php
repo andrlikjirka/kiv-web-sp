9fc14bf9b6f439aa3daa0293ed73133481899e86
Jiří Andrlík <andrlik.jirka@gmail.com>
2021-10-30 14:50:27 +0200

    add settings.inc.php

---
+++ Database/settings.inc.php
@@ -0,0 +1,21 @@
+<?php
+//////////////////////////////////////////////////////
+////////////// Zakladni nastaveni ////////////////////
+//////////////////////////////////////////////////////
+
+////// nastaveni pristupu k databazi ///////
+
+// prihlasovaci udaje k databazi
+define("DB_SERVER", "localhost");
+define("DB_NAME", "kiv-web-sp");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+
+
+// definice konkretnich nazvu tabulek
+define("TABLE_UZIVATELE", "jandrlik_uzivatele");
+define("TABLE_PRAVA", "jandrlik_prava");
+define("TABLE_PRISPEVKY", "jandrlik_prispevky");
+define("TABLE_HODNOCENI", "jandrlik_hodnoceni");
+
+
