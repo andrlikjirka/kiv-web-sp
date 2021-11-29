b8eea42096ff71b670ec37e2b38875313659e0fe
Jiří Andrlík <andrlik.jirka@gmail.com>
2021-11-29 15:55:13 +0100

    uprava vypisu pri nenacteni obsahu stranky

--- app/views/MyArticlesTemplate.tpl.php
+++ app/views/MyArticlesTemplate.tpl.php
@@ -11,12 +11,12 @@ global $tplData;
 
 ///////////// PRO NEPRIHLASENE UZIVATELE ///////////////
 if ($tplData['isUserLoggedIn'] == false) {
-    echo "<br><br><div class='alert alert-warning text-center mt-5' role='alert'>Spravovat články může jen přihlášený uživatel.</div>";
+    echo "<br><br><div class='alert alert-warning text-center mt-5' role='alert'>Psát články může jen přihlášený uživatel.</div>";
 
     ///////////// KONEC: PRO NEPRIHLASENE UZIVATELE ///////////////
 } else if ($tplData['isUserLoggedIn'] == true && $tplData['userData']['id_pravo'] != 4) {
     ///////////// PRO PRIHLASENE UZIVATELE BEZ PRAVA AUTOR ///////////////
-    echo "<br><br><div class='alert alert-warning text-center mt-5' role='alert'>Spravovat články může jen přihlášený uživatel s pravem alespoň Admin.</div>";
+    echo "<br><br><div class='alert alert-warning text-center mt-5' role='alert'>Psát články může jen přihlášený uživatel s pravem Autor.</div>";
 
     ///////////// KONEC: PRO PRIHLASENE UZIVATELE BEZ PRAVA AUTOR ///////////////
 
